190afd025ccad95f585e8ef5d2ed3882ad759495
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-08-01 12:25:14 +0500

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@
  limitations under the License.
  */
 
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import * as child_process from 'node:child_process';
 
@@ -31,9 +31,9 @@ const config = {
     adapter: adapter(),
 
     // https://kit.svelte.dev/docs/configuration#version
-    version: {
-      name: child_process.execSync('git rev-parse HEAD').toString().trim(),
-    },
+    // version: {
+    //   name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+    // },
   },
 };
 
